aaf3ba686cffc647f53b10fd77bf3a11575938d0
Kazu Yamamoto <kazu@iij.ad.jp>
2012-02-10 18:20:11 +0900

    removing stdout.

--- wai-logger-prefork/Network/Wai/Logger/Prefork.hs
+++ wai-logger-prefork/Network/Wai/Logger/Prefork.hs
@@ -2,9 +2,7 @@ module Network.Wai.Logger.Prefork (
     logCheck
   , logInit
   , logController
-  , ApacheLogger
   , LogType(..)
-  , IPAddrSource(..)
   , FileLogSpec(..)
   , LogController
   ) where
@@ -13,7 +11,6 @@ import Control.Concurrent
 import Control.Monad
 import Network.Wai.Logger
 import Network.Wai.Logger.Prefork.File
-import Network.Wai.Logger.Prefork.Stdout
 import Network.Wai.Logger.Prefork.Types
 import System.Log.FastLogger
 
@@ -24,7 +21,7 @@ logCheck (LogFile spec) = check spec
 
 logInit :: IPAddrSource -> LogType -> IO ApacheLogger
 logInit _ LogNone            = noLoggerInit
-logInit ipsrc LogStdout      = stdoutLoggerInit ipsrc
+logInit ipsrc LogStdout      = stdoutApacheLoggerInit ipsrc
 logInit ipsrc (LogFile spec) = fileLoggerInit ipsrc spec
 
 noLoggerInit :: IO ApacheLogger
--- wai-logger-prefork/Network/Wai/Logger/Prefork/Types.hs
+++ wai-logger-prefork/Network/Wai/Logger/Prefork/Types.hs
@@ -1,17 +1,12 @@
 module Network.Wai.Logger.Prefork.Types (
-    ApacheLogger
-  , FileLogSpec(..)
+    FileLogSpec(..)
   , LogType(..)
   , LogController
   ) where
 
-import Network.HTTP.Types
-import Network.Wai
 import System.Log.FastLogger
 import System.Posix (ProcessID)
 
 data LogType = LogNone | LogStdout | LogFile FileLogSpec
-
-type ApacheLogger = Request -> Status -> Maybe Integer -> IO ()
 
 type LogController = [ProcessID] -> IO ()
