5bb1df5faed622638797c7eb7156d84669d9239f
Kazu Yamamoto <kazu@iij.ad.jp>
2012-08-06 12:13:25 +0900

    using unix-time to speed up.

--- fast-logger/System/Log/FastLogger/Date.hs
+++ fast-logger/System/Log/FastLogger/Date.hs
@@ -1,63 +1,67 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP, OverloadedStrings #-}
 
 module System.Log.FastLogger.Date (
     ZonedDate
+  , WebDate
   , DateRef
   , dateInit
   , getDate
+  , getWebDate
   ) where
 
 import Control.Applicative
 import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as BS
 import Data.IORef
-import Data.Time
-import System.Locale
+import Data.UnixTime
 #if WINDOWS
 #define TIME UTCTime
 #define GETTIME getCurrentTime
 #else
 import System.Posix (EpochTime, epochTime)
-import Data.Time.Clock.POSIX
 #define TIME EpochTime
 #define GETTIME epochTime
 #endif
 
 -- | A type for zoned date.
 type ZonedDate = ByteString
+type WebDate = ByteString
 
 data DateCache = DateCache {
-    unixTime :: !TIME
+    unixTime  :: !TIME
   , zonedDate :: !ZonedDate
+  , webDate   :: !WebDate
   }
 
 -- | Reference to the 'ZonedDate' cache.
 newtype DateRef = DateRef (IORef DateCache)
 
--- | Getting 'ZonedDate' from the 'ZonedDate' cache.
+-- | Getting 'ZonedDate' from the cache.
 getDate :: DateRef -> IO ZonedDate
-getDate (DateRef ref) = do
+getDate dref = get zonedDate dref
+
+-- | Getting 'WebDate' from the cache.
+getWebDate :: DateRef -> IO ZonedDate
+getWebDate dref = get webDate dref
+
+get :: (DateCache -> a) -> DateRef -> IO a
+get getter (DateRef ref) = do
     newEt <- GETTIME
     cache <- readIORef ref
     let oldEt = unixTime cache
     if oldEt == newEt then
-        return $ zonedDate cache
+        return $ getter cache
       else do
-        newCache <- newDate newEt
+        let newCache = newDate newEt
         writeIORef ref newCache
-        return $ zonedDate newCache
+        return $ getter newCache
 
-newDate :: TIME -> IO DateCache
-newDate et = DateCache et . format <$> toZonedTime et
+newDate :: TIME -> DateCache
+newDate et = DateCache et zDate wDate
   where
-    toZonedTime = utcToLocalZonedTime . toUTC
-#if WINDOWS
-    toUTC = id
-#else
-    toUTC = posixSecondsToUTCTime . realToFrac
-#endif
-    format = BS.pack . formatTime defaultTimeLocale "%d/%b/%Y:%T %z"
+    ut = fromEpochTime et
+    zDate = formatUnixTime "%d/%b/%Y:%T %z" ut
+    wDate = formatUnixTimeGMT webDateFormat ut
 
 -- | Initializing the 'ZonedDate' cache.
 dateInit :: IO DateRef
-dateInit = DateRef <$> (GETTIME >>= newDate >>= newIORef)
+dateInit = DateRef <$> (newDate <$> GETTIME >>= newIORef)
