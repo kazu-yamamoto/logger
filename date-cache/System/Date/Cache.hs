7cb8211bb7f9ee655a52ce25dd83af7edd332376
Kazu Yamamoto <kazu@iij.ad.jp>
2012-08-31 15:05:43 +0900

    adding closer.

--- date-cache/System/Date/Cache.hs
+++ date-cache/System/Date/Cache.hs
@@ -1,38 +1,46 @@
 -- |
 -- Formatting time is slow.
--- This package provides mechanisms to cache formatted date
+-- This package provides mechanisms to cache formatted date.
 module System.Date.Cache (
-    Setting(..)
+  -- * Types
+    DateCacheConf(..)
+  , DateCacheGetter
+  , DateCacheCloser
+  -- * Date cacher
   , ondemandDateCacher
   , clockDateCacher
-    ) where
+  ) where
 
 import Control.Applicative
 import Control.Concurrent
-import Control.Monad
 import Data.ByteString (ByteString)
 import Data.IORef
 
+type DateCacheGetter = IO ByteString
+type DateCacheCloser = IO ()
+
 data DateCache t = DateCache {
-    timeKey   :: !t
+    timeKey :: !t
   , formattedDate :: !ByteString
   } deriving (Eq, Show)
 
-data Setting t = Setting {
+data DateCacheConf t = DateCacheConf {
     -- | A function to get a time. E.g 'epochTime' and 'getCurrentTime'.
     getTime :: IO t
     -- | A function to format a time.
-  , formatDate :: t -> ByteString
+  , formatDate :: t -> IO ByteString
   }
 
-newDate :: Setting t -> t -> IO (DateCache t)
-newDate setting tm = return $ DateCache tm (formatDate setting tm)
+newDate :: DateCacheConf t -> t -> IO (DateCache t)
+newDate setting tm = DateCache tm <$> formatDate setting tm
 
 -- |
 -- Date cacher which gets a time and formatted it only when
 -- returned getter is executed.
-ondemandDateCacher :: Eq t => Setting t -> IO (IO ByteString)
-ondemandDateCacher setting = getter <$> (getTime setting >>= newDate setting >>= newIORef)
+ondemandDateCacher :: Eq t => DateCacheConf t -> IO (DateCacheGetter, DateCacheCloser)
+ondemandDateCacher setting = do
+    ref <- getTime setting >>= newDate setting >>= newIORef
+    return $! (getter ref, closer)
   where
     getter ref = do
         newTm <- getTime setting
@@ -44,23 +52,26 @@ ondemandDateCacher setting = getter <$> (getTime setting >>= newDate setting >>=
             newCache <- newDate setting newTm
             writeIORef ref newCache
             return $ formattedDate newCache
+    closer = return ()
 
 -- |
 -- Date cacher which gets a time and formatted it every second.
 -- This returns a getter.
-clockDateCacher :: Eq t => Setting t -> IO (IO ByteString)
+clockDateCacher :: Eq t => DateCacheConf t -> IO (DateCacheGetter, DateCacheCloser)
 clockDateCacher setting = do
     ref <- getTime setting >>= newDate setting >>= newIORef
-    void . forkIO $ clock ref
-    return $ getter ref
+    tid <- forkIO $ clock ref
+    return $! (getter ref, closer tid)
   where
     getter ref = formattedDate <$> readIORef ref
     clock ref = do
         threadDelay 1000000
         tm <- getTime setting
+        date <- formatDate setting tm
         let new = DateCache {
                 timeKey = tm
-              , formattedDate = formatDate setting tm
+              , formattedDate = date
               }
         writeIORef ref new
         clock ref
+    closer tid = killThread tid
