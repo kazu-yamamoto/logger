d5af691f8ca497bee53e58b5f6bec3929428ab16
Kazu Yamamoto <kazu@iij.ad.jp>
2012-08-31 16:14:24 +0900

    fast-logger test clean up.

--- fast-logger/test/FastLoggerSpec.hs
+++ fast-logger/test/FastLoggerSpec.hs
@@ -1,43 +1,26 @@
 module FastLoggerSpec where
 
-import Control.Concurrent
 import Control.Exception (bracket)
-import qualified Data.ByteString.Char8 as BS
-import Data.IORef
 import System.IO
 import System.Log.FastLogger
 import Test.Hspec
-import Test.QuickCheck
-import Test.QuickCheck.Monadic
 
 spec :: Spec
 spec = describe "hPutLogStr" $ do
-    it "writes a log message" $ property prop_write
-    it "is safe for a large message" safeForLarge
-
-prop_write :: Property
-prop_write = mapSize (*100) write
+    it "is safe for a large message" $ safeForLarge [
+        100
+      , 1000
+      , 10000
+      , 100000
+      , 1000000
+      ]
 
 nullLogger :: IO Logger
 nullLogger = openFile "/dev/null" WriteMode >>= mkLogger True
 
-write :: String -> Property
-write xs = monadicIO $ do
-    res <- run $ bracket nullLogger rmLogger $ \lgr -> do
-        ref <- newIORef False
-        tid <- forkIO $ tryWrite ref lgr msg
-        threadDelay 1000
-        killThread tid
-        readIORef ref
-    assert res
-  where
-    msg = [LB (BS.pack xs)]
+safeForLarge :: [Int] -> IO ()
+safeForLarge ns = mapM_ safeForLarge' ns
 
-tryWrite :: IORef Bool -> Logger -> [LogStr] -> IO ()
-tryWrite ref lgr msg = do
-    loggerPutStr lgr msg
-    writeIORef ref True
-
-safeForLarge :: IO ()
-safeForLarge = bracket nullLogger rmLogger $ \lgr ->
-    loggerPutStr lgr [LS $ replicate 200000 'x']
+safeForLarge' :: Int -> IO ()
+safeForLarge' n = bracket nullLogger rmLogger $ \lgr ->
+    loggerPutStr lgr [LS $ replicate (abs n) 'x']
