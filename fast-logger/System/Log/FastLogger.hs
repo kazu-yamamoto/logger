960b506ca81c2d4df8dadfa56731dbde45ad0a4d
Kazu Yamamoto <kazu@iij.ad.jp>
2013-12-20 16:19:35 +0900

    Using ByteString builder when available.

--- fast-logger/System/Log/FastLogger.hs
+++ fast-logger/System/Log/FastLogger.hs
@@ -24,15 +24,22 @@ module System.Log.FastLogger (
   , module System.Log.FastLogger.File
   ) where
 
+#if MIN_VERSION_bytestring(0,10,2)
+import Data.ByteString.Builder (Builder, byteString)
+import Data.ByteString.Builder.Extra (Next(..))
+import qualified Data.ByteString.Builder.Extra as BBE
+#else
 import Blaze.ByteString.Builder.Internal.Types (Builder(..), BuildSignal(..), BufRange(..), runBuildStep, buildStep)
+import qualified Blaze.ByteString.Builder as BB
+import Foreign.Ptr (minusPtr)
+#endif
 import Control.Applicative ((<$>))
-import Control.Concurrent (getNumCapabilities, myThreadId, threadCapability, MVar, newMVar, takeMVar, putMVar)
+import Control.Concurrent (getNumCapabilities, myThreadId, threadCapability, MVar, newMVar, takeMVar, withMVar)
 import Control.Monad (when, replicateM)
 import Data.Array (Array, listArray, (!))
 import Data.ByteString.Internal (ByteString(..))
 import Data.IORef
 import Data.Monoid (Monoid, mempty, mappend)
-import qualified Blaze.ByteString.Builder as BB
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as S8
 import qualified Data.ByteString.Lazy as L
@@ -47,7 +54,7 @@ import qualified Data.Text.Lazy.Encoding as TL
 import Data.Word (Word8)
 import Foreign.ForeignPtr (withForeignPtr)
 import Foreign.Marshal.Alloc (mallocBytes, free)
-import Foreign.Ptr (Ptr, minusPtr, plusPtr)
+import Foreign.Ptr (Ptr, plusPtr)
 import GHC.IO.Device (close)
 import GHC.IO.FD (FD(..), openFile, writeRawBufferPtr)
 import GHC.IO.IOMode (IOMode(..))
@@ -58,6 +65,11 @@ import System.Log.FastLogger.File
 #if !MIN_VERSION_base(4,5,0)
 (<>) :: Monoid m => m -> m -> m
 (<>) = mappend
+#endif
+
+#if !MIN_VERSION_bytestring(0,10,2)
+byteString :: ByteString -> Builder
+byteString = BB.fromByteString
 #endif
 
 ----------------------------------------------------------------
@@ -82,7 +94,7 @@ data LogStr = LogStr {
   }
 
 instance Monoid LogStr where
-    mempty = LogStr 0 (BB.fromByteString BS.empty)
+    mempty = LogStr 0 (byteString BS.empty)
     LogStr s1 b1 `mappend` LogStr s2 b2 = LogStr (s1 + s2) (b1 <> b2)
 
 instance IsString LogStr where
@@ -106,7 +118,7 @@ instance ToLogStr TL.Text where
 
 -- | Creating 'LogStr'
 fromByteString :: ByteString -> LogStr
-fromByteString bs = LogStr (BS.length bs) (BB.fromByteString bs)
+fromByteString bs = LogStr (BS.length bs) (byteString bs)
 
 ----------------------------------------------------------------
 
@@ -122,16 +134,44 @@ writeLogStr fd buf size (LogStr len builder)
   | size < len = error "writeLogStr"
   | otherwise  = toBufIOWith buf size (write fd) builder
 
-toBufIOWith :: Buffer -> BufSize -> (Buffer -> Int -> IO a) -> Builder -> IO a
-toBufIOWith buf !size io (Builder build) = do
-    signal <- runBuildStep step bufRange
-    case signal of
-        Done ptr _ -> io buf (ptr `minusPtr` buf)
-        _          -> error "toBufIOWith"
+-- FIXME:
+-- | After this function call, Buffer should be empty
+#if MIN_VERSION_bytestring(0,10,2)
+toBufIOWith :: Buffer -> BufSize -> (Buffer -> Int -> IO ()) -> Builder -> IO ()
+toBufIOWith buf !size io builder = loop $ BBE.runBuilder builder
   where
-    !step = build (buildStep finalStep)
-    !bufRange = BufRange buf (buf `plusPtr` size)
+    loop writer = do
+        (len, next) <- writer buf size
+        io buf len
+        case next of
+             Done -> return ()
+             More minSize writer'
+               | size < minSize -> error "toBufIOWith: More: minSize"
+               | otherwise      -> loop writer'
+             Chunk (PS fptr off siz) writer'
+               | len == 0  -> loop writer' -- flushing
+               | otherwise -> withForeignPtr fptr $ \ptr -> io (ptr `plusPtr` off) siz
+#else
+toBufIOWith :: Buffer -> BufSize -> (Buffer -> Int -> IO ()) -> Builder -> IO ()
+toBufIOWith buf !size io (Builder build) = loop firstStep
+  where
+    firstStep = build (buildStep finalStep)
     finalStep !(BufRange p _) = return $ Done p ()
+    bufRange = BufRange buf (buf `plusPtr` size)
+    loop step = do
+        signal <- runBuildStep step bufRange
+        case signal of
+             Done ptr _ -> io buf (ptr `minusPtr` buf)
+             BufferFull minSize ptr next
+               | size < minSize -> error "toBufIOWith: BufferFull: minSize"
+               | otherwise      -> do
+                   io buf (ptr `minusPtr` buf)
+                   loop next
+             InsertByteString ptr (PS fptr off siz) next -> do
+                 io buf (ptr `minusPtr` buf)
+                 withForeignPtr fptr $ \p -> io (p `plusPtr` off) siz
+                 loop next
+#endif
 
 write :: FD -> Buffer -> Int -> IO ()
 write fd buf len' = loop buf (fromIntegral len')
@@ -153,10 +193,11 @@ newLogger size = do
     return $ Logger mbuf size lref
 
 pushLog :: FD -> Logger -> LogStr -> IO ()
-pushLog fd logger@(Logger  _ size ref) nlogmsg@(LogStr nlen nbuilder)
+pushLog fd logger@(Logger mbuf size ref) nlogmsg@(LogStr nlen nbuilder)
   | nlen > size = do
       flushLog fd logger
-      BB.toByteStringIO (writeByteString fd) nbuilder
+      -- FIXME: lock me!
+      withMVar mbuf $ \buf -> toBufIOWith buf nlen (write fd) nbuilder
   | otherwise = do
     needFlush <- atomicModifyIORef' ref checkBuf
     when needFlush $ do
@@ -167,10 +208,6 @@ pushLog fd logger@(Logger  _ size ref) nlogmsg@(LogStr nlen nbuilder)
       | size < olen + nlen = (ologmsg, True)
       | otherwise          = (ologmsg <> nlogmsg, False)
 
-writeByteString :: FD -> ByteString -> IO ()
-writeByteString fd (PS ps s l) = withForeignPtr ps $ \p ->
-    write fd (p `plusPtr` s) l
-
 flushLog :: FD -> Logger -> IO ()
 flushLog fd (Logger mbuf size lref) = do
     logmsg <- atomicModifyIORef' lref (\old -> (mempty, old))
@@ -180,9 +217,7 @@ flushLog fd (Logger mbuf size lref) = do
     -- there is no grantee that this function is exclusively called
     -- for a buffer. So, we use MVar here.
     -- This is safe and speed penalty can be ignored.
-    buf <- takeMVar mbuf
-    writeLogStr fd buf size logmsg
-    putMVar mbuf buf
+    withMVar mbuf $ \buf -> writeLogStr fd buf size logmsg
 
 ----------------------------------------------------------------
 
