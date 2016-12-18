ad696f112a5b33305888f0c863db1609279ea11b
Andrew Newman <andrewfnewman@gmail.com>
2016-12-18 19:12:20 +1000

    If win32 version less than 2.3.1.0 include fILE_APPEND_DATA constant

--- fast-logger/System/Log/FastLogger/FileIO.hs
+++ fast-logger/System/Log/FastLogger/FileIO.hs
@@ -12,6 +12,11 @@ import Graphics.Win32.Misc (getStdHandle, sTD_OUTPUT_HANDLE, sTD_ERROR_HANDLE)
 import Data.Bits ((.|.))
 
 type FD = HANDLE
+#ifdef !MIN_VERSION_win32(2,3,1,0)
+-- This flag is not defined in System.Win32.File
+fILE_APPEND_DATA :: UINT
+fILE_APPEND_DATA = 0x0004
+#endif
 
 -- This flag is not defined in System.Win32.File
 fILE_APPEND_DATA :: UINT
