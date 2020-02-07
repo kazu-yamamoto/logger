160fcf4fbd939fa3111b5e38cac15e523df8423a
Kazu Yamamoto <kazu@iij.ad.jp>
2020-02-07 12:26:56 +0900

    removing Imports from Internal.

--- fast-logger/System/Log/FastLogger/Internal.hs
+++ fast-logger/System/Log/FastLogger/Internal.hs
@@ -2,15 +2,12 @@
 -- |
 -- The contents of this module can change at any time without warning.
 module System.Log.FastLogger.Internal
-  ( module System.Log.FastLogger.Imports
-  , module System.Log.FastLogger.IO
+  ( module System.Log.FastLogger.IO
   , module System.Log.FastLogger.FileIO
   , module System.Log.FastLogger.LogStr
   , module System.Log.FastLogger.Logger
   ) where
 
-
-import System.Log.FastLogger.Imports
 import System.Log.FastLogger.IO
 import System.Log.FastLogger.FileIO
 import System.Log.FastLogger.LogStr
